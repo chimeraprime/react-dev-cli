9d82e8888b3b73dddb454e33fd40d7fd836056aa
Flixow <danielsiwek95@gmail.com>
2018-09-26 18:00:21 +0200

    [Component] Add tests to the rest of the methods, refactor tests

--- classes/Component.spec.js
+++ classes/Component.spec.js
@@ -11,13 +11,15 @@ const Component = require('./Component');
 const templates = require('../templates/component');
 
 describe('Component', () => {
+  const componentName = 'ComponentName';
+
   afterEach(mock.restore);
-  it('must be a class', () => {
+  it('should be a class', () => {
     expect(Component.prototype.constructor).to.be.a('function');
   });
 
 
-  it('must have set base path to components if exists', () => {
+  it('should have set base path to components if directory exists', () => {
     mock({
       'components': {},
     });
@@ -26,132 +28,136 @@ describe('Component', () => {
     expect(component.basePath).to.equal('./components');
   });
 
-  it('must have set base path to root if components directory doesn\'t exist', () => {
+  it('should have set base path to root if components directory doesn\'t exist', () => {
     const component = new Component();
 
     expect(component.basePath).to.equal('');
   });
 
-  describe('must generate proper template based on options', () => {
-    it('no options', () => {
-      const component = new Component('ComponentName');
-      const template = component.buildTemplate();
+  describe('[buildTemplate]', () => {
+    describe('should generate proper template based on options', () => {
+      it('no options', () => {
+        const component = new Component('ComponentName');
+        const template = component.buildTemplate();
 
-      const expectedImports = [templates.imports.react];
-      const expectedBody = [templates.main].join('\n');
-      const expectedExport = [templates.exported.default];
-      const expectedTemplate = expectedImports.join('\n') + '\n' + expectedBody + '\n' + expectedExport;
+        const expectedImports = [templates.imports.react];
+        const expectedBody = [templates.main].join('\n');
+        const expectedExport = [templates.exported.default];
+        const expectedTemplate = expectedImports.join('\n') + '\n' + expectedBody + '\n' + expectedExport;
 
-      expect(template).to.equal(expectedTemplate);
-    });
+        expect(template).to.equal(expectedTemplate);
+      });
 
-    it('withConnect', () => {
-      const component = new Component('ComponentName', { withConnect: true });
-      const template = component.buildTemplate();
+      it('withConnect', () => {
+        const component = new Component('ComponentName', { withConnect: true });
+        const template = component.buildTemplate();
 
-      const expectedImports = [templates.imports.react, templates.imports.connect];
-      const expectedBody = [templates.main].join('\n');
-      const expectedExport = [templates.exported.withConnect];
-      const expectedTemplate = expectedImports.join('\n') + '\n' + expectedBody + '\n' + expectedExport;
+        const expectedImports = [templates.imports.react, templates.imports.connect];
+        const expectedBody = [templates.main].join('\n');
+        const expectedExport = [templates.exported.withConnect];
+        const expectedTemplate = expectedImports.join('\n') + '\n' + expectedBody + '\n' + expectedExport;
 
-      expect(template).to.equal(expectedTemplate);
-    });
+        expect(template).to.equal(expectedTemplate);
+      });
 
-    it('style', () => {
-      const component = new Component('ComponentName', { style: true });
-      const template = component.buildTemplate();
+      it('style', () => {
+        const component = new Component('ComponentName', { style: true });
+        const template = component.buildTemplate();
 
-      const expectedImports = [templates.imports.react, '\n' + templates.imports.stylesheet];
-      const expectedBody = [templates.main].join('\n');
-      const expectedExport = [templates.exported.default];
-      const expectedTemplate = expectedImports.join('\n') + '\n' + expectedBody + '\n' + expectedExport;
+        const expectedImports = [templates.imports.react, '\n' + templates.imports.stylesheet];
+        const expectedBody = [templates.main].join('\n');
+        const expectedExport = [templates.exported.default];
+        const expectedTemplate = expectedImports.join('\n') + '\n' + expectedBody + '\n' + expectedExport;
 
-      expect(template).to.equal(expectedTemplate);
-    });
+        expect(template).to.equal(expectedTemplate);
+      });
 
-    it('withConnect and style', () => {
-      const component = new Component('ComponentName', { withConnect: true, style: true });
-      const template = component.buildTemplate();
+      it('withConnect and style', () => {
+        const component = new Component(componentName, { withConnect: true, style: true });
+        const template = component.buildTemplate();
 
-      const expectedImports = [
-        templates.imports.react,
-        templates.imports.connect,
-        '\n' + templates.imports.stylesheet,
-      ];
-      const expectedBody = [templates.main].join('\n');
-      const expectedExport = [templates.exported.withConnect];
-      const expectedTemplate = expectedImports.join('\n') + '\n' + expectedBody + '\n' + expectedExport;
+        const expectedImports = [
+          templates.imports.react,
+          templates.imports.connect,
+          '\n' + templates.imports.stylesheet,
+        ];
+        const expectedBody = [templates.main].join('\n');
+        const expectedExport = [templates.exported.withConnect];
+        const expectedTemplate = expectedImports.join('\n') + '\n' + expectedBody + '\n' + expectedExport;
 
-      expect(template).to.equal(expectedTemplate);
+        expect(template).to.equal(expectedTemplate);
+      });
     });
   });
 
-  describe('must write component structure', () => {
-    it('with component file if doesn\'t exist', () => {
-      const component = new Component('ComponentName');
-      const template = 'sample template';
+  describe('[writeComponentStructure]', () => {
+    let component;
+    let writeComponentFileStub;
+    let writeComponentIndexFileStub;
+    let manageComponentsIndexFileStub;
 
-      const writeComponentFileStub = sinon.stub(component, 'writeComponentFile');
-      const writeComponentIndexFileStub = sinon.stub(component, 'writeComponentIndexFile');
-      const manageComponentsIndexFileStub = sinon.stub(component, 'manageComponentsIndexFile');
-
-      component.writeComponentStructure(template);
-
-      expect(writeComponentFileStub).to.have.been.calledWith(template);
-      expect(writeComponentIndexFileStub).to.have.been.called;
-      expect(manageComponentsIndexFileStub).to.have.been.called;
+    beforeEach(() => {
+      component = new Component(componentName);
+      writeComponentFileStub = sinon.stub(component, 'writeComponentFile');
+      writeComponentIndexFileStub = sinon.stub(component, 'writeComponentIndexFile');
+      manageComponentsIndexFileStub = sinon.stub(component, 'manageComponentsIndexFile');
+    });
+    afterEach(() => {
+      writeComponentFileStub.restore();
+      writeComponentIndexFileStub.restore();
+      manageComponentsIndexFileStub.restore();
     });
 
-    it('without component file if doesn\'t exist', () => {
-      const name = 'ComponentName';
-      mock({
-        [`/${name}/${name}.js`]: Buffer.from([8, 6, 7, 5, 3, 0, 9]),
+    describe('should write component structure', () => {
+      it('with component file if doesn\'t exist', () => {
+        const template = 'sample template';
+
+        component.writeComponentStructure(template);
+
+        expect(writeComponentFileStub).to.have.been.calledWith(template);
+        expect(writeComponentIndexFileStub).to.have.been.called;
+        expect(manageComponentsIndexFileStub).to.have.been.called;
       });
-      const component = new Component(name);
 
-      const writeComponentFileStub = sinon.stub(component, 'writeComponentFile');
-      const writeComponentIndexFileStub = sinon.stub(component, 'writeComponentIndexFile');
-      const manageComponentsIndexFileStub = sinon.stub(component, 'manageComponentsIndexFile');
+      it('without component file if already exists', () => {
+        mock({
+          [`/${componentName}/${componentName}.js`]: Buffer.from([8, 6, 7, 5, 3, 0, 9]),
+        });
 
-      component.writeComponentStructure('sample template');
+        component.writeComponentStructure('sample template');
 
-      expect(writeComponentFileStub).to.have.not.been.called;
-      expect(writeComponentIndexFileStub).to.have.been.called;
-      expect(manageComponentsIndexFileStub).to.have.been.called;
-    });
+        expect(writeComponentFileStub).to.have.not.been.called;
+      });
 
-    it('with styles file', () => {
-      const component = new Component('ComponentName', { style: true });
+      it('with styles file', () => {
+        component.options.style = true;
+        const writeStylesFileStub = sinon.stub(component, 'writeStylesFile');
 
-      sinon.stub(component, 'writeComponentFile');
-      sinon.stub(component, 'writeComponentIndexFile');
-      sinon.stub(component, 'manageComponentsIndexFile');
-      const writeStylesFileStub = sinon.stub(component, 'writeStylesFile');
+        component.writeComponentStructure('sample template');
 
-      component.writeComponentStructure('sample template');
-
-      expect(writeStylesFileStub).to.have.been.called;
+        expect(writeStylesFileStub).to.have.been.called;
+      });
     });
   });
 
   describe('[writeStylesFile]', () => {
-    const name = 'ComponentName';
-    const componentPath = `/${name}/${name}.scss`;
+    const componentPath = `/${componentName}/${componentName}.scss`;
+    let outputFileSyncStub;
+    let component;
 
-    it('must create empty scss file if doesn\'t exist', () => {
-      const component = new Component(name, { style: true });
-      const outputFileSyncStub = sinon.stub(fs, 'outputFileSync');
+    beforeEach(() => {
+      outputFileSyncStub = sinon.stub(fs, 'outputFileSync');
+      component = new Component(componentName, { style: true });
+    });
+    afterEach(() => outputFileSyncStub.restore());
 
+    it('should create empty scss file if doesn\'t exist', () => {
       component.writeStylesFile();
 
       expect(fs.outputFileSync).to.have.been.calledWith(componentPath, '');
-      outputFileSyncStub.restore();
     });
 
-    it('must not create scss file if already exists', () => {
-      const component = new Component(name, { style: true });
-      const outputFileSyncStub = sinon.stub(fs, 'outputFileSync');
-
+    it('should not create scss file if already exists', () => {
       mock({
         [componentPath]: Buffer.from([8, 6, 7, 5, 3, 0, 9]),
       });
@@ -159,7 +165,105 @@ describe('Component', () => {
       component.writeStylesFile();
 
       expect(fs.outputFileSync).to.have.not.been.called;
-      outputFileSyncStub.restore();
+    });
+  });
+
+  describe('[writeComponentIndexFile]', () => {
+    const indexPath = `/${componentName}/index.js`;
+    let outputFileStub;
+    let component;
+
+    beforeEach(() => {
+      outputFileStub = sinon.stub(fs, 'outputFile');
+      component = new Component(componentName);
+    });
+    afterEach(() => outputFileStub.restore());
+
+    it('should create index file for a component, if doesn\'t exist', () => {
+      component.writeComponentIndexFile();
+
+      expect(outputFileStub).to.have.been.calledWith(indexPath, templates.indexes.default);
+    });
+
+    it('should not create index file for component if already exists', () => {
+      mock({
+        [indexPath]: Buffer.from([8, 6, 7, 5, 3, 0, 9]),
+      });
+
+      component.writeComponentIndexFile();
+
+      expect(outputFileStub).to.have.not.been.called;
+    });
+  });
+
+  describe('[manageComponentsIndexFile]', () => {
+    const indexPath = 'components/index.js';
+    let component;
+    let updateComponentsIndexFileStub;
+    let createComponentsIndexFileStub;
+
+    beforeEach(() => {
+      mock({
+        components: {},
+      });
+      component = new Component(componentName);
+      updateComponentsIndexFileStub = sinon.stub(component, 'updateComponentsIndexFile');
+      createComponentsIndexFileStub = sinon.stub(component, 'createComponentsIndexFile');
+    });
+    afterEach(() => {
+      mock.restore();
+    });
+
+    it('should update components index file if already exists', () => {
+      mock({
+        components: {
+          ['index.js']: {},
+        },
+      });
+
+      component.manageComponentsIndexFile();
+
+      expect(updateComponentsIndexFileStub).to.have.been.calledWithMatch(indexPath);
+      expect(createComponentsIndexFileStub).to.have.not.been.called;
+    });
+
+    it('should create components index file if doesn\'t exist', () => {
+      component.manageComponentsIndexFile();
+
+      expect(createComponentsIndexFileStub).to.have.been.calledWithMatch(indexPath);
+      expect(updateComponentsIndexFileStub).to.have.not.been.called;
+    });
+  });
+
+  describe('[updateComponentsIndexFile]', () => {
+    const path = 'components/index.js';
+    const indexBaseContent = "export { default as AnotherComponent } from './AnotherComponent'";
+    let outputFileStub;
+    let readFileStub;
+    let component;
+
+    beforeEach(() => {
+      component = new Component(componentName);
+      outputFileStub = sinon.stub(fs, 'outputFile');
+      readFileStub = sinon.stub(fs, 'readFile')
+        .callsFake((path, format, cb) => cb('', indexBaseContent + '\n'));
+    });
+    afterEach(() => {
+      outputFileStub.restore();
+      readFileStub.restore();
+    });
+
+    it('should update file if content is not duplicated', () => {
+      const content = `export { default as ${componentName} } from './${componentName}'`;
+      const expectedContent = `${indexBaseContent}\n${content}\n`;
+
+      component.updateComponentsIndexFile(path, content);
+      expect(outputFileStub).to.have.been.calledWithMatch(path, expectedContent);
+    });
+
+    it('should not update file if content is duplicated', () => {
+      component.updateComponentsIndexFile(path, indexBaseContent);
+      expect(outputFileStub).to.have.not.been.called;
     });
   });
 });
